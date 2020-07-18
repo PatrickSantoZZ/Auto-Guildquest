a08d8834894deae2ec2faaae0458556fcaf070ee
Nekonekoo <patrickfetscher60@gmail.com>
2020-07-18 19:04:07 +0200

    reworked for 95.05 , new settings , added cli-mode

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 'use strict'
 String.prototype.clr = function (hexColor) { return `<font color='#${hexColor}'>${this}</font>` };
+ const fs = require('fs'),
+	   path = require('path'),		
+       Quests = require("./quests.json");
 
-const SettingsUI = require('tera-mod-ui').Settings;
-const Quests = require("./quests.json");
+const settings = JSON.parse(fs.readFileSync(path.join(__dirname, "config.json")));
+if (!settings.enabled) return;
 
 module.exports = function AutoGuildquest(mod) {
+	const { command } = mod.require;
+
 
 	let myQuestId = 0,
 		status = 2,
@@ -16,8 +21,8 @@ module.exports = function AutoGuildquest(mod) {
 		weekly = 0,
 		niceName = mod.proxyAuthor !== 'caali' ? '[VG] ' : ''
 	  
-	mod.game.me.on('change_zone', (zone, quick) => {
-		if (mod.settings.battleground.includes(zone)) {
+	mod.hook('S_LOAD_TOPO',3, (event) => {
+		if (settings.battleground.includes(event.zone)) {
 			hold = true
 		} else if (hold && myQuestId !== 0) {
 			hold = false
@@ -28,22 +33,22 @@ module.exports = function AutoGuildquest(mod) {
 	});
 
 //Hook
-	mod.game.on('enter_game', () => {daily = weekly = 0})
+	mod.hook('S_LOGIN', 'raw', () => {daily = 0 , weekly = 0})
 	mod.hookOnce('S_AVAILABLE_EVENT_MATCHING_LIST', 1, event => {daily = event.unk4weekly = event.unk6})
-	mod.hook('S_LOGIN', 'event', () => {mod.hookOnce('S_SPAWN_ME', 'event', () => {setTimeout(dailycredit,1000+ Math.random()*250);});});
+	mod.hook('S_LOGIN', 'raw', () => {mod.hookOnce('S_SPAWN_ME', 'raw', () => {setTimeout(dailycredit,1000+ Math.random()*250);});});
 	mod.hook('S_FIELD_EVENT_ON_ENTER', 'raw', () => {  entered = true;});
 	mod.hook('C_RETURN_TO_LOBBY', 'raw', () => {  entered = false;});
 	mod.hook('S_COMPLETE_EVENT_MATCHING_QUEST', 1, (event) => {
 		daily++
 		weekly++
-		if (mod.settings.Vanguard) {
+		if (settings.Vanguard) {
 			myQuestId = event.id
 			if (!hold) setTimeout(completeQuest,1000+ Math.random()*250);
 		}
 	});
-	mod.hook('S_FIELD_EVENT_PROGRESS_INFO', 1, () => {if (mod.settings.Guardian) setTimeout(completeGuardian, 2000+ Math.random()*250);});
+	mod.hook('S_FIELD_EVENT_PROGRESS_INFO', 1, () => {if (settings.Guardian) setTimeout(completeGuardian, 2000+ Math.random()*250);});
 	mod.hook('S_UPDATE_GUILD_QUEST_STATUS', 1, (event) => {
-		if (mod.settings.GQuest) {
+		if (settings.GQuest) {
 			if (event.targets[0].completed == event.targets[0].total) {
 				setTimeout(()=>{
 				mod.send('C_REQUEST_FINISH_GUILD_QUEST', 1, {quest: event.quest})
@@ -62,11 +67,12 @@ module.exports = function AutoGuildquest(mod) {
 			name: 'Guardian Mission',
 			message: String(event.cleared + " / 40")
 			});}clr = event.cleared;});
-/*	 mod.hook("S_GUILD_QUEST_LIST", 1, (event) => {
-		if (mod.settings.GQuestLog) {
+	 mod.hook("S_GUILD_QUEST_LIST", 2, (event) => {
+		if (settings.GQuestLog) {
 			GetQuestsInfo(event["quests"]);
 		}
-	})*/
+	})
+
 //Function
 	function completeQuest() {
 		mod.send('C_COMPLETE_DAILY_EVENT', 1, {id: myQuestId})	
@@ -75,12 +81,12 @@ module.exports = function AutoGuildquest(mod) {
 		setTimeout(() => {mod.send('C_COMPLETE_EXTRA_EVENT', 1, {type: 1})
 		}, 1000+ Math.random()*250)
 		myQuestId = 0
-		if(mod.settings.VLog) report() 
+		if(settings.VLog) report() 
 		
 	}; 
 	function report() {
-		if(daily < 16) mod.command.message(niceName + 'Daily Vanguard Requests completed: ' + daily)
-		else mod.command.message(niceName + 'You have completed all 16 Vanguard Requests today.')
+		if(daily < 16) sendMessage(niceName + 'Daily Vanguard Requests completed: ' + daily)
+		else sendMessage(niceName + 'You have completed all 16 Vanguard Requests today.')
 	}
 	function completeGuardian() {
 		mod.send('C_REQUEST_FIELD_POINT_REWARD', 1, {})
@@ -89,20 +95,21 @@ module.exports = function AutoGuildquest(mod) {
 	}, 2000+ Math.random()*500)
 };
 	function dailycredit() {
-		if (mod.settings.Daily) {
-			let _ = mod.trySend('C_REQUEST_RECV_DAILY_TOKEN', 1, {});
-			 !_ ? mod.log('Unmapped protocol packet \<C_REQUEST_RECV_DAILY_TOKEN\>.') : null;
+		if (settings.Daily) {
+			let _ = mod.send('C_REQUEST_RECV_DAILY_TOKEN', 1, {});
+			 !_ ? sendMessage('Unmapped protocol packet \<C_REQUEST_RECV_DAILY_TOKEN\>.') : null;
 		  }
 	};
-/*	function GetQuestsInfo(questEvent) {
+	function GetQuestsInfo(questEvent) {
 		for (let questIndex in questEvent) {
 			if ([1, 2].includes(questEvent[questIndex]["status"])) {
 				let qName = questEvent[questIndex]["name"].replace("@GuildQuest:", "");
 				let qSize = GetQuestSize(questEvent[questIndex]["size"]);
 				let qStatus = `${questEvent[questIndex]["status"] == 1 ? "[ACTIVE]".clr("f1ef48") : "[COMPLETE]".clr("3fce29")}`;
 				let qTime = new Date(1000 * questEvent[questIndex]["timeRemaining"]).toISOString().substr(11, 8);
-				mod.command.message(`${qStatus} ${Quests[qName].clr("0cccd6")} ${qSize.clr("0c95d4")} Time left: ${qTime.clr("db3dce")}`)
-		} else {continue}}} 
+				sendMessage(`${qStatus} ${Quests[qName].clr("0cccd6")} ${qSize.clr("0c95d4")} Time left: ${qTime.clr("db3dce")}`)
+		} else {continue}}}
+
 	function GetQuestSize(size) {
 		if (size == 0) {
 			return "(Small)"
@@ -111,53 +118,51 @@ module.exports = function AutoGuildquest(mod) {
 		} else {
 			return "(Large)"
 		}
-	}*/
-	function sendMessage(msg) { mod.command.message(msg) }
-	let ui = null;
-	if (global.TeraProxy.GUIMode) {
-		ui = new SettingsUI(mod, require('./settings_structure'), mod.settings, { alwaysOnTop: true, width: 550, height: 232 });
-		ui.on('update', settings => { mod.settings = settings; });
-
-		this.destructor = () => {
-			if (ui) {
-				ui.close();
-				ui = null;
-			}
-		};
 	}
+
+	function sendMessage(msg) { 
+		if(settings.CLI){ mod.log(msg) }
+		else { command.message(msg) }
+}
+
 //Command
-	mod.command.add('auto', {
+	command.add('auto', {
+		'AC': () =>{
+		   settings.enabled = !settings.enabled
+		   sendMessage("AutoQuests-Mod is now : " + (settings.enabled ? "On" : "Off"));
+		},
 		'VG': () => {
-			mod.settings.Vanguard = !mod.settings.Vanguard
-			sendMessage("Auto-Vanguardquest: " + (mod.settings.Vanguard ? "On" : "Off"));
+			settings.Vanguard = !settings.Vanguard
+			sendMessage("Auto-Vanguardquest: " + (settings.Vanguard ? "On" : "Off"));
 		},
 		'GQ': () => {
-			mod.settings.GQuest = !mod.settings.GQuest
-			sendMessage("Auto-Guildquest: " + (mod.settings.GQuest ? "On" : "Off"));
+			settings.GQuest = !settings.GQuest
+			sendMessage("Auto-Guildquest: " + (settings.GQuest ? "On" : "Off"));
 		},
 		'GL': () => {
-			mod.settings.Guardian = !mod.settings.Guardian
-			sendMessage("Auto-Gardian-Legion: " + (mod.settings.Guardian ? "On" : "Off"));
+			settings.Guardian = !settings.Guardian
+			sendMessage("Auto-Guardian-Legion: " + (settings.Guardian ? "On" : "Off"));
 		},
 		'DC': () => {
-			mod.settings.Daily = !mod.settings.Daily
-			sendMessage("Auto-Daily-Credit: " + (mod.settings.Daily ? "On" : "Off"));
+			settings.Daily = !settings.Daily
+			sendMessage("Auto-Daily-Credit: " + (settings.Daily ? "On" : "Off"));
 		},
 		'VGLog': () => {
-			mod.settings.VLog = !mod.settings.VLog
-			sendMessage("Vanguard-Quest Logger: " + (mod.settings.VLog ? "On" : "Off"));
+			settings.VLog = !settings.VLog
+			sendMessage("Vanguard-Quest Logger: " + (settings.VLog ? "On" : "Off"));
 		},
-		'UI': () => {
-			ui.show();
+		'CLI': () => {
+			settings.CLI = !settings.CLI
+			sendMessage("CLI is now " + (settings.CLI ? "On" : "Off"));
 		},
 		'$default': () => {
 			sendMessage(`Invalid argument. usasge command with 'auto'`),
-			sendMessage(`UI | Show the ui setting`),
 			sendMessage(`VQ | Auto-Vanguard`),
 			sendMessage(`GQ | Auto-GuildQuest with relaunch`),
 			sendMessage(`VGLog |Vanguard-Quest-Logger`),
-			sendMessage(`GL |Auto claim box in Gardian legion`),
-			sendMessage(`DL |Auto claim Daily cradit `);
+			sendMessage(`GL |Auto claim box in Guardian legion`),
+			sendMessage(`DL |Auto claim Daily credits`);
+			sendMessage(`CLI | activated/deactivated Clientless mode`)
 		}
 	});
 	}
